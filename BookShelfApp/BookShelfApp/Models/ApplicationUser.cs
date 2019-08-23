52b0bb6623e47a08cff381b0a8ab65e6b33dae61
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-08-23 13:59:35 -0500

    creating DB, working on data seeding

--- BookShelfApp/BookShelfApp/Models/ApplicationUser.cs
+++ BookShelfApp/BookShelfApp/Models/ApplicationUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,10 +7,8 @@ using System.Threading.Tasks;
 
 namespace BookShelfApp.Models
 {
-    public class ApplicationUser
+    public class ApplicationUser : IdentityUser
     {
-        public int Id { get; set; }
-
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
